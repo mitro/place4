c8758f1a452bff96f69486706b041364894252b2
mitro <dmtr.melnik@gmail.com>
2024-11-18 21:03:05 +0500

    Create solution and TelegramBot project

---
+++ Place3.TelegramBot/Program.cs
@@ -0,0 +1,22 @@
+﻿using Telegram.Bot;
+using Telegram.Bot.Types;
+using Telegram.Bot.Types.Enums;
+
+using var cts = new CancellationTokenSource();
+var bot = new TelegramBotClient("YOUR_BOT_TOKEN", cancellationToken: cts.Token);
+var me = await bot.GetMe();
+bot.OnMessage += OnMessage;
+
+Console.WriteLine($"@{me.Username} is running... Press Enter to terminate");
+Console.ReadLine();
+cts.Cancel();
+
+async Task OnMessage(Message msg, UpdateType type)
+{
+    if (msg.Text is null)
+    {
+        return;
+    }
+    Console.WriteLine($"Received {type} '{msg.Text}' in {msg.Chat}");
+    await bot.SendMessage(msg.Chat, $"{msg.From} said: {msg.Text}");
+}
